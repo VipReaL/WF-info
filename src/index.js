b89ec9fe49fd294850265a97f68705ddf31939cf
Sergey Igoshin <VipReaLWeb@gmail.com>
2024-11-11 20:10:56 +0500

    js

--- src/index.js
+++ src/index.js
@@ -20,32 +20,67 @@ getButton.addEventListener('click', function () {
 
 // dropDownMenu
 
-// const nav = document.querySelector('.site-navigation');
+// находим раздел навигации
+const nav = document.querySelector('.site-navigation');
+nav.classList.add('enhanced');
+// console.log(nav)
 
-// // Находим все выподающие списки .menu
-// const submenus = nav.querySelectorAll('.menu');
+// находим кнопки раскрытия меню
+const submenus = nav.querySelectorAll('.menu-dropdown[data-has-children]');
+// console.log(submenus);
 
-// // Скрываем все выподающие меню (hidden)
-// submenus.forEach((item) => {
-//   item.setAttribute('hidden', '')
-// });
+// находим выподающий список меню
+const dropdowns = nav.querySelectorAll('.menu-dropdown[data-has-children] > .menu-dropdown__list');
+// console.log(dropdowns);
 
-// const submenusButton = nav.querySelectorAll('.menu__btn');
 
-// submenusButton.forEach((item) => {
-//   item.addEventListener('click', function (event) {
-//     const dropdown = event.target.closest('.site-navigation__item').querySelector('.menu');
+submenus.forEach((item) => {
+  const dropdown = item.querySelector(':scope > .menu-dropdown__list');
+  // console.log(dropdown);
+  
+  // Скрываем все выподающие меню при загрузке страницы (hidden)
+  dropdown.setAttribute('hidden', '');
 
-//     toggleDropeDownMenu(item, dropdown);
-//   });
-// });
+  const button = item.querySelector(':scope > .menu-dropdown__btn')
+  // console.log(button);
 
-// function toggleDropeDownMenu (button, dropdown) {
-//   if (button.getAttribute('aria-expanded') === 'true') {
-//     button.setAttribute('aria-expanded', 'false');
-//     dropdown.setAttribute('hidden', '');
-//   } else {
-//     button.setAttribute('aria-expanded', 'true');
-//     dropdown.removeAttribute('hidden');
-//   }
-// }
+  // раскрываем-скрываем меню при клике
+  button.addEventListener('click', function () {
+    toggleDropdown(button, dropdown);
+  })
+
+  // Обрабатываем нажатие на Esc
+
+
+
+});
+
+function toggleDropdown (button, dropdown) {
+  if (button.getAttribute('aria-expanded') === 'true') {
+    button.setAttribute('aria-expanded', 'false');
+    dropdown.setAttribute('hidden', '');
+  } else {
+    button.setAttribute('aria-expanded', 'true');
+    dropdown.removeAttribute('hidden');
+  }
+}
+
+function focusIsInside(element) {
+  return element.contains(document.activeElement)
+}
+
+// Закрываем навигацию, если кликнули вне навигации
+function collapseDropdownsWhenClickingOutsideNav(e) {
+  const target = e.target
+  
+  dropdowns.forEach(function (dropdown) {
+    if (!dropdown.parentElement.contains(target)) {
+      dropdown.setAttribute('hidden', '')
+      const btn = dropdown.parentNode.querySelector('.menu-dropdown__btn')
+      btn.setAttribute('aria-expanded', 'false')
+    }
+  });
+}
+
+// Закрываем навигацию, если кликнули вне навигации
+window.addEventListener('click', collapseDropdownsWhenClickingOutsideNav)
