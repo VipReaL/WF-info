7337660ba13ebdf50b3cb4d20d0cc8071c6560b4
Sergey Igoshin <VipReaLWeb@gmail.com>
2024-11-11 16:11:44 +0500

    header.css

--- src/index.js
+++ src/index.js
@@ -17,3 +17,35 @@ getButton.addEventListener('click', function () {
     .then((data) => console.log(data))
     // сюда подставить функцию и передать ей нужные пораметры из data
 })
+
+// dropDownMenu
+
+// const nav = document.querySelector('.site-navigation');
+
+// // Находим все выподающие списки .menu
+// const submenus = nav.querySelectorAll('.menu');
+
+// // Скрываем все выподающие меню (hidden)
+// submenus.forEach((item) => {
+//   item.setAttribute('hidden', '')
+// });
+
+// const submenusButton = nav.querySelectorAll('.menu__btn');
+
+// submenusButton.forEach((item) => {
+//   item.addEventListener('click', function (event) {
+//     const dropdown = event.target.closest('.site-navigation__item').querySelector('.menu');
+
+//     toggleDropeDownMenu(item, dropdown);
+//   });
+// });
+
+// function toggleDropeDownMenu (button, dropdown) {
+//   if (button.getAttribute('aria-expanded') === 'true') {
+//     button.setAttribute('aria-expanded', 'false');
+//     dropdown.setAttribute('hidden', '');
+//   } else {
+//     button.setAttribute('aria-expanded', 'true');
+//     dropdown.removeAttribute('hidden');
+//   }
+// }
